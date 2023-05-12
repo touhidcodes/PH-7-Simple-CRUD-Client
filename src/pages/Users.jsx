690535dd88c6b9e94653fa366b4a7fd8c04e1c2d
Touhidur Zaman <touhidcodes@gmail.com>
2023-05-12 21:49:21 +0600

    delete-data

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
-	const users = useLoaderData();
+	const loadedUsers = useLoaderData();
+	const [users, setUsers] = useState(loadedUsers);
 	console.log(users);
+
+	const handleDelete = (id) => {
+		fetch(`http://localhost:5000/user/${id}`, {
+			method: "DELETE",
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				if (data.deletedCount > 0) {
+					alert("deleted successfully");
+					const remaining = users.filter((user) => user._id !== id);
+					setUsers(remaining);
+				}
+			});
+	};
 	return (
 		<div>
 			<h1>Users of MongoDB</h1>
@@ -11,7 +27,8 @@ const Users = () => {
 			<div>
 				{users.map((user) => (
 					<p key={user._id}>
-						{user.name} : {user.email}
+						{user.name} : {user.email}{" "}
+						<button onClick={() => handleDelete(user._id)}>X</button>
 					</p>
 				))}
 			</div>
