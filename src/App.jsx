508a4e97ab06d59b9fb8b0e5baa78d02e881dcb8
Touhidur Zaman <touhidcodes@gmail.com>
2023-05-12 16:27:13 +0600

    post-data-to-mongodb

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const handleUser = (event) => {
+		event.preventDefault();
+		const form = event.target;
+		const name = form.name.value;
+		const email = form.email.value;
+		const user = { name, email };
+		// form.reset();
+		console.log(user);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+		fetch("http://localhost:5000/users", {
+			method: "POST",
+			headers: {
+				"content-type": "application/json",
+			},
+			body: JSON.stringify(user),
+		})
+			.then((res) => res.json())
+			.then((data) => console.log(data));
+	};
+	return (
+		<>
+			<h1>Simple CRUD</h1>
+			<form onSubmit={handleUser}>
+				<input type='text' name='name' id='' placeholder='name' /> <br />
+				<input type='email' name='email' id='' placeholder='email' />
+				<br />
+				<input type='submit' value='Add User' />
+			</form>
+		</>
+	);
 }
 
-export default App
+export default App;
